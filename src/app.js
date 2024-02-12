60693e9d91b44a24234521a2d4b946a00df4c2b1
scg-code <samgifford.work@gmail.com>
2024-02-12 20:22:30 +1000

    Set up basic boilerplate for Deliverease Back-end

--- src/app.js
+++ src/app.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000; 
 
-// Later: Connect to your MongoDB database with your database URL replacing the placeholder
+// Later: Connect to MongoDB database with database URL replacing the placeholder
 mongoose.connect('mongodb://localhost:27017/your_database_name')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB', err)); 
 
-// Later: Import and mount your routes (more on this in 2 weeks )
+// Later: Import and mount routes 
 
 app.listen(port, () => {
     console.log(`Server is listening on http://localhost:${port}`);
