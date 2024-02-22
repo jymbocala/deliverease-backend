90d6502f9233d07219dad43fb3952832f63ba23c
jymbocala <jym.bocala@gmail.com>
2024-02-22 14:50:34 +1100

    add uploading to aws s3 logic

---
+++ s3.js
@@ -0,0 +1,61 @@
+// Import the AWS SDK
+const AWS = require('aws-sdk');
+
+// Configure the AWS SDK with your credentials
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
+
+// Function to handle uploading to AWS S3
+async function uploadToS3(file) {
+  try {
+    // Create a new instance of the S3 service
+    const s3 = new AWS.S3({
+      params: {
+        Bucket: process.env.S3_BUCKET_NAME,
+      },
+      region: "ap-southeast-2",
+    });
+
+    // Generate a unique file key based on the current timestamp and file name
+    const fileKey = `uploads/${Date.now().toString()}-${file.name.replace(/ /g, '-')}`;
+
+    // Set up parameters for the S3 upload
+    const params = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: fileKey,
+      Body: file,
+    };
+
+    // Perform the upload to S3
+    const upload = s3.upload(params).promise();
+
+    // Log progress during the upload
+    upload.on('httpUploadProgress', evt => {
+      console.log("Uploading to S3...", parseInt(((evt.loaded * 100) / evt.total).toString()) + "%");
+    });
+
+    // Wait for the upload to complete
+    await upload;
+
+    // Return file information
+    return {
+      fileKey,
+      fileName: file.name,
+    };
+  } catch (error) {
+    console.error("Error uploading file to S3:", error);
+    throw error;
+  }
+}
+
+// Function to get the publicly accessible S3 URL of a file
+function getS3Url(fileKey) {
+  return `https://${process.env.S3_BUCKET_NAME}.s3.ap-southeast-2.amazonaws.com/${fileKey}`;
+}
+
+module.exports = {
+  uploadToS3,
+  getS3Url,
+};
