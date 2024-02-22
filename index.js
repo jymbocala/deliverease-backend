1c7eaaa462640dd986797490a341f893fbdb6c0e
scg-code <samgifford.work@gmail.com>
2024-02-23 09:13:19 +1000

    added models for users and locations

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user_routes.js';
 
 const app = express();
 dotenv.config();
@@ -9,18 +10,22 @@ dotenv.config();
 app.use(express.json());
 
 // Routes
-// Define your routes here
+app.get('/', (req, res) => {
+        res.send('Hello World');
+});
+
+app.use('/users', userRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.DB_URI)
-  .then(() => {
-    console.log('Connected to MongoDB');
-    // Start the server
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://127.0.0.1:${PORT}`);
+    .then(() => {
+        console.log('Successfully connected to MongoDB Atlas');
+        // Start the server
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://127.0.0.1:${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB Atlas:', error.message);
     });
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
---
+++ models/location.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const locationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    dockNumber: {
+        type: Number,
+        required: false,
+    },
+    dockHours: {
+        type: String,
+        required: false,
+    },
+    parking: {
+        type: String,
+        required: false,
+    },
+    contactName: {
+        type: String,
+        required: false,
+    },
+    contactNumber: {
+        type: Number,
+        required: false,
+    },
+    notes: {
+        type: String,
+        required: false,
+    },
+    }
+);
+
+const LocationModel = mongoose.model('Location', locationSchema);
+
+export default LocationModel;
+
