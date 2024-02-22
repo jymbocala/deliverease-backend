211d33bce5c3d03988bcc12abea81be38a91dd18
scg-code <samgifford.work@gmail.com>
2024-02-22 13:26:45 +1000

    boilerplate back up

--- index.js
+++ index.js
@@ -12,16 +12,13 @@ app.use(express.json());
 // Define your routes here
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.DB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     // Start the server
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+      console.log(`Server is running on http://127.0.0.1:${PORT}`);
     });
   })
   .catch((error) => {
